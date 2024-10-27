86d04fdc3cc4d81c58059d89982591733dae9633
Alexios33 <alexiostry@hotmail.com>
2024-10-27 00:30:20 +0000

    Initial commit

---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,60 @@
-import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import svgrPlugin from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  server: {
-    port: Number(process.env.PORT) || 3000,
-    strictPort: true,
-    host: true,
-    https: true,
-    watch: {
-      usePolling: false,
-      useFsEvents: false
-    },
-    hmr: {
-      overlay: false
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, './src/components'),
+      config: path.resolve(__dirname, './src/config'),
+      contracts: path.resolve(__dirname, './src/contracts'),
+      localConstants: path.resolve(__dirname, './src/localConstants'),
+      pages: path.resolve(__dirname, './src/pages'),
+      routes: path.resolve(__dirname, './src/routes'),
+      wrappers: path.resolve(__dirname, './src/wrappers'),
+      types: path.resolve(__dirname, './src/types'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+      'hooks/sdkDappHooks': path.resolve(__dirname, './src/hooks/sdkDappHooks'),
+      utils: path.resolve(__dirname, './src/utils'),
+      'utils/getCallbackRoute': path.resolve(__dirname, './src/utils/getCallbackRoute'),
+      'utils/sdkDappUtils': path.resolve(__dirname, './src/utils/sdkDappUtils'),
+      'utils/sdkDappCore': path.resolve(__dirname, './src/utils/sdkDappCore'),
+      'utils/smartContract': path.resolve(__dirname, './src/utils/smartContract'),
+      assets: path.resolve(__dirname, './src/assets'),
+      helpers: path.resolve(__dirname, './src/helpers'),
+      'helpers/sdkDappHelpers': path.resolve(__dirname, './src/helpers/sdkDappHelpers'),
+      services: path.resolve(__dirname, './src/services'),
+      'services/sdkDappServices': path.resolve(__dirname, './src/services/sdkDappServices')
     }
   },
   plugins: [
-    react(),
-    basicSsl(),
-    tsconfigPaths(),
-    svgrPlugin(),
-    nodePolyfills({
-      globals: { Buffer: true, global: true, process: true }
-    })
-  ],
-  build: {
-    outDir: 'build'
-  },
-  preview: {
-    port: 3002,
-    https: true,
-    host: 'localhost',
-    strictPort: true
-  }
-});
+      react(),
+      svgr(),
+      basicSsl()
+    ],
+    server: {
+      port: 3000,
+      strictPort: true,
+      host: '0.0.0.0',
+      https: true,
+      watch: {
+        usePolling: true
+      },
+      hmr: {
+        clientPort: 443,
+        host: '0.0.0.0',
+        protocol: 'ws',
+        timeout: 120000
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+      }
+    },
+    define: {
+      'process.env': process.env
+    }
+  });
